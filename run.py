00b5e63935446ad93964977bf5c5de00113bde41
Drahushchak <dragushhakigor@gmail.com>
2019-11-23 20:37:22 +0200

    fix

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from main import Twit_Scraper
+from twit_scrap import Twit_Scraper
 import json
 
 twit_scraper = Twit_Scraper()
 distribution = twit_scraper.get_freq_dist('#зе', 'зе')
-with open('data.json', 'w', indent=4, encoding='utf-8') as f:
-    json.dump(distribution, f, ensure_ascii=False)
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(distribution, f, indent=4, ensure_ascii=False)
